be223e913470def39a1e63eb3ef1d57df69db8a9
vinkyWS <vinky.wijaya@gmail.com>
2015-06-06 07:23:59 +0700

    Tugas 1 PD2

---
+++ unit1.pas
@@ -0,0 +1,51 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
+  StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    Label1: TLabel;
+    Panel1: TPanel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if
+  button1.caption='Tampilkan' then
+  begin
+  showmessage(edit1.text);
+  button1.caption:='Keluar';
+  end else
+  if
+  button1.caption='Keluar' then
+  application.terminate;
+  end;
+
+end.
+
